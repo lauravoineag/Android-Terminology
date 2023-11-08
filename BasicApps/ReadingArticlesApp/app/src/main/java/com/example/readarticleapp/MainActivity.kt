e8082dc4d65b2c12c45de71d332956a7d4966cfd
Laura Voineag <laura.voineag@bbc.co.uk>
2023-11-08 09:30:08 +0000

    first page finished of Articles app

--- BasicApps/ReadingArticlesApp/app/src/main/java/com/example/readarticleapp/MainActivity.kt
+++ BasicApps/ReadingArticlesApp/app/src/main/java/com/example/readarticleapp/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.CombinedModifier
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,42 +32,56 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TopImage("Laura")
+                    ComposeArticleApp()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun TopImage(message: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.bg_compose_background)
-    Box{ Image(painter = image, contentDescription =null ) }
-    Column {
-        Text1(message, modifier)
-        Text2(message = message, modifier = modifier)
-        Text3(message = message, modifier = modifier)
+fun ComposeArticleApp() {
+    ArticleCard(
+        title = stringResource(R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(R.string.compose_short_desc),
+        longDescription = stringResource(R.string.compose_long_desc),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+private fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Text1(message: String, modifier: Modifier) {
-    Text(text = "Jetpack Compose tutorial", fontSize = 24.sp)
-    modifier.padding(16.dp)
-    TextAlign.Justify
+fun ComposeArticleAppPreview() {
+    ReadArticleAppTheme {
+        ComposeArticleApp()
+    }
 }
-
-@Composable
-fun Text2(message: String, modifier: Modifier) {
-    Text(text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.")
-    modifier.padding(start = 16.dp, end = 16.dp)
-    TextAlign.Justify
-}
-
-@Composable
-fun Text3(message: String, modifier: Modifier) {
-    Text(text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
-    modifier.padding(16.dp)
-    TextAlign.Justify
-}
-
