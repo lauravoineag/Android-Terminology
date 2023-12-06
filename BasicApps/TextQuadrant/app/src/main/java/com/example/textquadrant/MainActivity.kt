2e9372cc896713ecd6d92e5c49504ef59550b9ef
Laura Voineag <laura.voineag@bbc.co.uk>
2023-12-06 16:15:51 +0000

    Add Text Quadrant App

---
+++ BasicApps/TextQuadrant/app/src/main/java/com/example/textquadrant/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.textquadrant
+
+import android.icu.text.IDNA.Info
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.textquadrant.ui.theme.TextQuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TextQuadrantTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    QuadrantApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun QuadrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            InfoCard(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                backgroundColour = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f))
+            InfoCard(
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                backgroundColour = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f))
+        }
+
+        Row(Modifier.weight(1f)) {
+            InfoCard(
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_description),
+                backgroundColour = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_description),
+                backgroundColour = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoCard(title: String, description: String, backgroundColour: Color, modifier: Modifier)
+{
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColour)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = title, modifier = Modifier.padding(bottom = 16.dp),fontWeight = FontWeight.Bold)
+        Text(text = description, textAlign = TextAlign.Justify)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrantAppPreview() {
+    TextQuadrantTheme {
+        InfoCard("Blue","something blue", backgroundColour = Color.Green, modifier = Modifier)
+    }
+}
+
+
